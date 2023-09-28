71c58d4b5dc6c1afa5df97014f059d431aab98b1
MahmoudAlsayes <mahmoudalsayes03@gmail.com>
2023-09-29 02:46:23 +0300

    finsh TabBar

--- lib/screen2.dart
+++ lib/screen2.dart
@@ -1,11 +1,44 @@
 import 'package:flutter/material.dart';
 
+///TabBar
+
 class SecondScreen extends StatelessWidget {
-  static const String routename= "third";
+  static const String routename = "second";
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Center(child: Text("TabBar")),
+          bottom: TabBar(
+            tabs: [
+              Tab(
+                text: "Cat",icon: Icon(Icons.home),
+              ),
+              Tab(
+                text: "Dog",icon: Icon(Icons.list),
+              ),
+              Tab(
+                text: "Caw",icon: Icon(Icons.history),
+              ),
+            ],
+
+          ),
+
+        ),
+        body: TabBarView(children:
+        [
+          Image(image: AssetImage("assets/images/1.jpeg")),
+          // Image.asset("assets/images/1.jpeg"),
+          Image.asset("assets/images/2.jpeg"),
+          Image.asset("assets/images/3.jpeg"),
+        ]
+        ),
+      ),
+
+    );
+
   }
 }
-
